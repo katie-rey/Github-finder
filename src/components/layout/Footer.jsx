949e4828d25d57b4ed6fbab1da335b67a92f667b
Katie-Rey <katiejorey@gmail.com>
2022-06-14 18:13:19 +1200

    Inital layout for Githubb finder API

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  const footerYear = new Date().getFullYear()
+
+  return (
+    <footer className="footer p-10 bg-grey-700 text-primary-content footer-center">
+      <div>
+        <p>Copyright &copy; {footerYear} </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import { FaHome } from 'react-icons/fa'
+'
+import React from 'react'
+
+function NotFound() {
+  return <div>NotFunds</div>
+}
+
+export default NotFound
