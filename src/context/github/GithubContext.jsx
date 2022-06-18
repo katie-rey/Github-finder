e6d323e4a798b6752c4124f4680d919bbb2e2690
Katie-Rey <katiejorey@gmail.com>
2022-06-18 15:37:39 +1200

    Development of user profile page

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -35,6 +35,28 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  //Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
   //Clear users
   const clearUsers = () => {
     dispatch({
@@ -58,6 +80,7 @@ export const GithubProvider = ({ children }) => {
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}
