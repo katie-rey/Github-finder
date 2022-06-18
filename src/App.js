efd65ad6e04929fe34a5d73acbd9d9f0e43fb86f
Katie-Rey <katiejorey@gmail.com>
2022-06-18 14:09:04 +1200

    Render Github profiles on page using useContext hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 import NotFound from './components/pages/NotFound'
 import Home from './components/pages/Home'
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
+import User from './components/pages/User'
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/notfound" element={<NotFound />}></Route>
-            <Route path="/*" element={<NotFound />}></Route>
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/notfound" element={<NotFound />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+                <Route path="/user/:login}" element={<User />}></Route>
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   )
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import UserResults from '../users/UserResults'
+import UserSearch from '../users/UserSearch'
 
 function Home() {
   return (
     <div>
       <h1 className="text-6xl">Welcome</h1>
+      {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
+      <UserSearch />
+      <UserResults />
     </div>
   )
 }
